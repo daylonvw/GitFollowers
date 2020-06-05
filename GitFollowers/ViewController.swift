cd6492f3b9a9d7bbc09ae14913e37a5b4be2219c
Daylon van Wel <daylon-van.wel@klm.com>
2020-06-05 14:31:12 +0200

    Initial Commit

---
+++ GitFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitFollowers
+//
+//  Created by Daylon van wel on 05/06/2020.
+//  Copyright © 2020 Daylon van Wel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
